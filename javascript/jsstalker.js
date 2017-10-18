ab720d1c32bb08cc800e779fe065eaeb1e0edda8
cmsegui <cmsegui@yahoo.com>
2017-10-18 17:33:11 -0400

    did a small exercise putting together very basic js

---
+++ javascript/jsstalker.js
@@ -0,0 +1,6 @@
+let firstName = prompt("What is your first name?");
+let lastName = prompt("What is your last name?");
+let age = prompt("What is your age?");
+
+console.log("Hi, my name is " + firstName + " " + lastName + ".")
+console.log("I am " + age + " years old.")
