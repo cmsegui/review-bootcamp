0288cec55c1b8f07ca2697dc6e32c8507fe61bcc
cmsegui <cmsegui@yahoo.com>
2017-10-29 16:02:16 -0400

    refactored loop and made separate functions, used splice and added a delete function

--- javascript/list.js
+++ javascript/list.js
@@ -4,11 +4,30 @@ let input = prompt("What would you like to do?");
 
 while (input !== "quit") {
     if (input === "list") {
-        console.log(todos);
+        listTodos();
     } else if (input === "new") {
-        const newTodo = prompt("Enter new todo");
-        todos.push(newTodo);
+        addTodo();
+    } else if (input === "delete") {
+        deleteTodo();
     }
     input = prompt("What would you like to do?");
 }
 console.log("Ok, you quit the app");
+
+function listTodos() {
+    todos.forEach(function (todo, i) {
+        console.log(i + ": " + todo);
+    });
+}
+
+function addTodo() {
+    const newTodo = prompt("Enter new todo");
+    todos.push(newTodo);
+    console.log("New todo has been added");
+}
+
+function deleteTodo() {
+    let index = prompt("Enter index of todo to be deleted")
+    todos.splice(index, 1);
+    console.log("Todo has been deleted")
+}
