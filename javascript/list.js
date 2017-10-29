9010880a0cd2cfcbe88590583d8f6fbc6948e406
cmsegui <cmsegui@yahoo.com>
2017-10-29 13:06:16 -0400

    practiced adding to arrays and loops by making todo list only in console log

---
+++ javascript/list.js
@@ -0,0 +1,14 @@
+let todos = ["Make the bed"];
+
+let input = prompt("What would you like to do?");
+
+while (input !== "quit") {
+    if (input === "list") {
+        console.log(todos);
+    } else if (input === "new") {
+        const newTodo = prompt("Enter new todo");
+        todos.push(newTodo);
+    }
+    input = prompt("What would you like to do?");
+}
+console.log("Ok, you quit the app");
