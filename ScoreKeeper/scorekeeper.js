735e9d1254efc9d20ecdc05c2cc60b322f2153e7
cmsegui <cmsegui@yahoo.com>
2017-11-01 11:35:45 -0400

    practiced more dom manipulation with reset button and changing the winning score

--- ScoreKeeper/scorekeeper.js
+++ ScoreKeeper/scorekeeper.js
@@ -1,7 +1,11 @@
 let p1Button = document.querySelector("#p1");
 let p2Button = document.querySelector("#p2");
+let resetButton = document.querySelector("#reset");
 let p1Display = document.querySelector("#p1Display");
 let p2Display = document.querySelector("#p2Display");
+let numInput = document.querySelector("input")
+let winningScoreDisplay = document.querySelector("p span")
+let p = document.querySelector("p");
 let p1Score = 0;
 let p2Score = 0;
 let gameOver = false;
@@ -11,18 +15,41 @@ p1Button.addEventListener("click", function () {
     if (!gameOver) {
         p1Score++;
         if (p1Score === winningScore) {
+            p1Display.classList.add("winner")
             gameOver = true;
         }
         p1Display.textContent = p1Score;
     }
-})
+});
 
 p2Button.addEventListener("click", function () {
     if (!gameOver) {
         p2Score++;
         if (p2Score === winningScore) {
             gameOver = true;
+            p2Display.classList.add("winner")
+            
         }
         p2Display.textContent = p2Score;
     }
-})
+});
+
+function reset() {
+    p1Score = 0;
+    p2Score = 0;
+    p1Display.textContent = 0;
+    p2Display.textContent = 0;
+    p1Display.classList.remove("winner");
+    p2Display.classList.remove("winner");
+    gameOver = false;
+};
+
+resetButton.addEventListener("click", function () {
+    reset();
+});
+
+numInput.addEventListener("change", function () {
+    winningScoreDisplay.textContent = this.value;
+    winningScore = Number(this.value);
+    reset();
+});
